5c8149dfb05a62f9d9801dbf1553d2b4d8e2a8c7
Nitesh <niteshmali132002@gmail.com>
2023-03-08 01:04:15 +0530

    Input in Ruby

---
+++ 02_Input.rb
@@ -0,0 +1,7 @@
+puts "Enter a number : "
+
+# we use gets to read the input from the user.
+# The chomp method is used to remove any trailing newline character from the input
+num = gets.chomp
+
+puts "You entered number #{num}"
