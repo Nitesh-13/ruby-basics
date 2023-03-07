32e4d24712830ce1c6b8d44e3d15cee32de3068e
Nitesh <niteshmali132002@gmail.com>
2023-03-08 01:04:58 +0530

    Functions in Ruby

---
+++ 06_Functions.rb
@@ -0,0 +1,15 @@
+# Functions in Ruby
+
+def add(x,y)
+    return x+y
+end
+
+def sub(x,y)
+    return x-y
+end
+
+a = 10
+b = 5
+
+puts "Addition of 10 and 5 is #{add(a,b)}"
+puts "Difference of 10 and 5 is #{sub(a,b)}"
