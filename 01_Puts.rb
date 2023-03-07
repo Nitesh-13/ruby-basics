1c90176dba0a3a9b5b4612b729c2329e5f5e00da
Nitesh <niteshmali132002@gmail.com>
2023-03-08 01:04:04 +0530

    Print Statement

---
+++ 01_Puts.rb
@@ -0,0 +1,19 @@
+# Puts Statement is used to print anything on onsole window
+puts "Hello!"
+puts "Welcome to Ruby Programming!"
+
+
+# Declaration of variables
+x = 90
+y = 10
+
+puts "The Addition of X and Y is " , x+y
+puts "The Difference of X and Y is " , x-y
+puts "The Multiplication of X and Y is " , x*y
+puts "The Division of X and Y is " , x/y
+
+
+name = "Nitesh"
+
+# we use string interpolation to insert the value of the name variable into the string
+puts "Hello, My Name is #{name}"
